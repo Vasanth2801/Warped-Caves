d16e2fee8b40029b53da1599dee85a5e5efe7d3f
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-09 10:12:39 +0530

    Object Pooler

---
+++ Warped Carves/Assets/Scripts/ObjectPooling/ObjectPooler.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class ObjectPoolItem
+    {
+        public string objectTag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public ObjectPoolItem[] pools;
+    public Dictionary<string, Queue<GameObject>> poolsOfDictionary;
+
+    private void Start()
+    {
+        poolsOfDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(ObjectPoolItem pool in pools)
+        {
+            Queue<GameObject> objPool = new Queue<GameObject>();
+
+            for(int i = 0;i< pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objPool.Enqueue(obj);
+            }
+        }
+    }
+
+    public GameObject SpawnFromPools(string tag, Vector3 position, Quaternion rotation)
+    {
+        GameObject objToSpawn = poolsOfDictionary[tag].Dequeue();
+        objToSpawn.transform.position = position;
+        objToSpawn.transform.rotation = rotation;
+
+        poolsOfDictionary[tag].Enqueue(objToSpawn);
+
+        return objToSpawn;
+    }
+}
