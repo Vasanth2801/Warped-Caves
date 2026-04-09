8aeb8cc1c4e2929c60b530305b07a5eaeef99325
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-09 09:26:58 +0530

    Player Movement

---
+++ Warped Carves/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 13f;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Inputs")]
+    [SerializeField] private float moveInput;
+
+    private void Update()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+}
